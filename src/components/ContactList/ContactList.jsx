f25d40dadccf38b6bf27a9cb0845ebc7d89caa6e
deathfluffy <mrandreu454@gmail.com>
2024-01-28 14:15:50 +0200

    ++++

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,31 +1,17 @@
 import { useSelector } from 'react-redux';
-import { useMemo } from 'react';
-import { getContacts } from '../../redux/Contact/ContactsSlice';
-import { getFilter } from '../../redux/ContactFilter/ContactFilter';
-import { ContactItem } from 'components/ContactItem/ContactItem';
+import ContactItem from 'components/ContactItem/ContactItem';
 import css from './ContactList.module.css';
+import { selectVisibleContacts } from '../../redux/selectors';
 
-export default function ContactList  ()  {
-  const contacts = useSelector(getContacts);
-  const filterValue = useSelector(getFilter);
-
-  const getFilteredContacts = useMemo(() => () => {
-    const normalizedFilter = filterValue.toLowerCase().trim();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  }, [contacts, filterValue]);
-
-  const filteredContacts = getFilteredContacts();
-
-
+export default function ContactList() {
+  const contacts = useSelector(selectVisibleContacts);
+  
   return (
     <ul className={css.list}>
-      {filteredContacts.map(({ id, name, number }) => (
-        <ContactItem key={id} id={id} name={name} number={number} />
+      {contacts.map(contact => (
+        <ContactItem key={contact.id} contact={contact} />
       ))}
     </ul>
   );
-};
-
+}
 
--- src/redux/Contact/ContactsSlice.js
+++ src/redux/Contact/ContactsSlice.js
@@ -1,29 +1,52 @@
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 
-import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContacts, deleteContacts } from '../Services/Api';
 
-const initialState = {
-  items: [],
-  isLoading: false,
-  error: null
-};
+
+const getActions = type =>
+  isAnyOf(fetchContacts[type], addContacts[type], deleteContacts[type]);
+
+
+const initialState = { items: [], isLoading: false, error: null };
+
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState,
-  reducers: {
-    addContact: (state, action) => {
-      state.contacts.push(action.payload);
-      window.localStorage.setItem('contacts', JSON.stringify(state.contacts));
-    },
-    removeContact: (state, action) => {
-      state.contacts = state.contacts.filter(contact => contact.id !== action.payload);
-      window.localStorage.setItem('contacts', JSON.stringify(state.contacts));
-    },
-  },
+  initialState, 
+  extraReducers: builder =>
+    builder
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+
+        state.items = action.payload;
+      })
+      .addCase(addContacts.fulfilled, (state, action) => {
+    
+        state.items.unshift(action.payload);
+      })
+      .addCase(deleteContacts.fulfilled, (state, action) => {
+   
+        const index = state.items.findIndex(
+          contact => contact.id === action.payload.id
+        );
+        state.items.splice(index, 1);
+      })
+      .addMatcher(getActions('pending'), state => {
+  
+        state.isLoading = true;
+      })
+      .addMatcher(getActions('rejected'), (state, action) => {
+   
+        state.isLoading = false; 
+        state.error = action.payload; 
+      })
+      .addMatcher(getActions('fulfilled'), state => {
+
+        state.isLoading = false; 
+        state.error = null; 
+      }),
 });
 
-export const { addContact, removeContact } = contactsSlice.actions;
+
+export const { addContact, deleteContact } = contactsSlice.actions;
 
 export const contactsReducer = contactsSlice.reducer;
-export const getContacts = state => state.contactsScope.contacts;
-
